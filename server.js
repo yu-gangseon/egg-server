0d4d0b7c678d1eeb243516e80fb2779a9e900170
yu-gangseon <85759734+yu-gangseon@users.noreply.github.com>
2026-02-27 13:12:57 +0900

    Update server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ const contract = new ethers.Contract(
 /* ========================
    IPFS 메타데이터
 ======================== */
-const METADATA_URI = "ipfs://bafkreiaucpmqeu6ztdhhm55gnbpdmjejjvmdnoorrl6mts4wzicfmnw4jm"; // 
+const METADATA_URI = "bafkreid75y3v6w2salzrwnjiuf7e3inyhxhz5jyuv62efvx5ndrljlsoeu"; // 
 
 /* ========================
    쿨다운 저장
